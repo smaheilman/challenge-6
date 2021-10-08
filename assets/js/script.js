8db95de595628fe5f47e854045510573311c3323
Shmee <smaheilman@gmail.com>
2021-10-07 22:20:28 -0400

    icons added

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,8 @@ var cityInput = document.querySelector("#search");
 var cityFormEl = document.querySelector("#city-form")
 var weatherDisplay = document.querySelector("#weather-display");
 var weatherSearchTerm = document.querySelector("#weather-search-term");
-var forecastEl = document.querySelector(".five-day");
+var forecastEl = document.querySelector(".forecast");
+var forecastE1 = document.querySelector("#dayOne")
 var forecastE2 = document.querySelector("#dayTwo");
 var forecastE3 = document.querySelector("#dayThree");
 var forecastE4 = document.querySelector("#dayFour");
@@ -21,7 +22,7 @@ var formSubmitHandler = function (event) {
         getWeatherCity(city);
         weatherDisplay.textContent = "";
         cityInput.value = "";
-        forecastEl.textContent="";
+        forecastE1.textContent="";
         forecastE2.textContent='';
         forecastE3.textContent='';
         forecastE4.textContent='';
@@ -54,20 +55,26 @@ var getWeatherCity = function (city) {
     console.log(city);
         
     cities.push(city);
-
+    
+    cities = cities.concat(JSON.parse(localStorage.getItem("searchcity")))
     localStorage.setItem("searchcity", JSON.stringify(cities));
 
-    var saved = JSON.parse(localStorage.getItem("searchcity"));
-    console.log(saved);
+    console.log(cities);
 
 };
 
 
 var displayWeather = function (data, searchTerm) {
+
+    var currentIconImg = document.createElement("img");
+    currentIconImg.setAttribute('src', "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+    weatherDisplay.appendChild(currentIconImg);
+
+
     var date= moment.unix(data.dt).format("MM/DD/YYYY");
 
     weatherSearchTerm.textContent = "Weather in " + searchTerm + " " + date;
-
+    console.log(data.weather[0].icon);
     var getWeather = function (lat, long) {
         var lat = (data.coord.lat);
         var long = (data.coord.lon);
@@ -102,57 +109,83 @@ var displayWeather = function (data, searchTerm) {
     var displayForecast1 = function (data) {
         var date= moment.unix(data.daily[1].dt).format("MM/DD/YYYY");
 
-        let forecast = [date, "Temp: " + data.daily[1].temp.day + "F", data.daily[1].weather[0].icon, "Wind Speed: " + data.daily[1].wind_speed + "mph", "Humidty: " + data.daily[1].humidity + "%"]
+        let forecast = [date, "Temp: " + data.daily[1].temp.day + "F", "Wind Speed: " + data.daily[1].wind_speed + "mph", "Humidty: " + data.daily[1].humidity + "%"]
         let list = document.getElementById("dayOne")
         forecast.forEach((item) => {
             let li = document.createElement("li");
             li.innerText= item;
             list.appendChild(li);
         }) 
+
+        var iconImg = document.createElement("img");
+        iconImg.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[1].weather[0].icon + "@2x.png");
+        forecastE1.appendChild(iconImg);
     }
     var displayForecast2 = function (data) {
-        var date= moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
+        
 
-        let forecast = [date, "Temp: " + data.daily[2].temp.day + "F", data.daily[2].weather[0].icon, "Wind Speed: " + data.daily[2].wind_speed + "mph", "Humidty: " + data.daily[2].humidity + "%"]
+        var date= moment.unix(data.daily[2].dt).format("MM/DD/YYYY");
+        
+        
+
+        let forecast = [date, "Temp: " + data.daily[2].temp.day + "F", "Wind Speed: " + data.daily[2].wind_speed + "mph", "Humidty: " + data.daily[2].humidity + "%"]
         let list = document.getElementById("dayTwo")
         forecast.forEach((item) => {
             let li = document.createElement("li");
             li.innerText= item;
             list.appendChild(li);
         }) 
+        
+        var iconImg = document.createElement("img");
+        iconImg.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[2].weather[0].icon + "@2x.png");
+        forecastE2.appendChild(iconImg);
     }
     var displayForecast3 = function (data) {
         var date= moment.unix(data.daily[3].dt).format("MM/DD/YYYY");
 
-        let forecast = [date, "Temp: " + data.daily[3].temp.day + "F", data.daily[3].weather[0].icon, "Wind Speed: " + data.daily[3].wind_speed + "mph", "Humidty: " + data.daily[3].humidity + "%"]
+        let forecast = [date, "Temp: " + data.daily[3].temp.day + "F", "Wind Speed: " + data.daily[3].wind_speed + "mph", "Humidty: " + data.daily[3].humidity + "%"]
         let list = document.getElementById("dayThree")
         forecast.forEach((item) => {
             let li = document.createElement("li");
             li.innerText= item;
             list.appendChild(li);
         }) 
+
+        var iconImg = document.createElement("img");
+        iconImg.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[3].weather[0].icon + "@2x.png");
+        forecastE3.appendChild(iconImg);
+
     }
     var displayForecast4 = function (data) {
         var date= moment.unix(data.daily[4].dt).format("MM/DD/YYYY");
 
-        let forecast = [date, "Temp: " + data.daily[4].temp.day + "F", data.daily[4].weather[0].icon, "Wind Speed: " + data.daily[4].wind_speed + "mph", "Humidty: " + data.daily[4].humidity + "%"]
+        let forecast = [date, "Temp: " + data.daily[4].temp.day + "F", "Wind Speed: " + data.daily[4].wind_speed + "mph", "Humidty: " + data.daily[4].humidity + "%"]
         let list = document.getElementById("dayFour")
         forecast.forEach((item) => {
             let li = document.createElement("li");
             li.innerText= item;
             list.appendChild(li);
         }) 
+
+        var iconImg = document.createElement("img");
+        iconImg.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[4].weather[0].icon + "@2x.png");
+        forecastE4.appendChild(iconImg);
+
     }
     var displayForecast5 = function (data) {
         var date= moment.unix(data.daily[5].dt).format("MM/DD/YYYY");
 
-        let forecast = [date, "Temp: " + data.daily[5].temp.day + "F", data.daily[5].weather[0].icon, "Wind Speed: " + data.daily[5].wind_speed + "mph", "Humidty: " + data.daily[5].humidity + "%"]
+        let forecast = [date, "Temp: " + data.daily[5].temp.day + "F", "Wind Speed: " + data.daily[5].wind_speed + "mph", "Humidty: " + data.daily[5].humidity + "%"]
         let list = document.getElementById("dayFive")
         forecast.forEach((item) => {
             let li = document.createElement("li");
             li.innerText= item;
             list.appendChild(li);
         }) 
+
+        var iconImg = document.createElement("img");
+        iconImg.setAttribute('src', "http://openweathermap.org/img/wn/" + data.daily[5].weather[0].icon + "@2x.png");
+        forecastE5.appendChild(iconImg);
     }
     getWeather();
 }
